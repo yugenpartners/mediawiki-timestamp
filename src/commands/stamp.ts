e20fe26fb7bc257ceb60caad04d6ceb0366fe991
Cory Francis Myers <cfm@panix.com>
2021-04-06 14:42:56 -0700

    chore: lint

--- src/commands/stamp.ts
+++ src/commands/stamp.ts
@@ -3,26 +3,28 @@ import * as libxmljs from 'libxmljs';
 import * as xml2js from 'xml2js';
 import { Command, flags } from '@oclif/command';
 
+const xmlnsPrefix: string = 'mw';
+
 export default class Stamp extends Command {
   static description = 'timestamp a MediaWiki export';
   static args = [
     { name: 'export', description: 'MediaWiki ".xml" export', required: true },
   ];
 
-  static prefix = 'mw';
-
   async run() {
     const { args, flags } = this.parse(Stamp);
 
     // Load and parse the ".xml" export.
-    const data = await fs.readFileSync(args.export);
-    const doc = <libxmljs.Document>libxmljs.parseXml(data);
-    this.debug(`xmlns=${this.ns(doc).mw}`);
+    const buf = await fs.readFileSync(args.export);
+    const doc = <libxmljs.Document>libxmljs.parseXml(buf.toString());
+    this.debug(`xmlns=${this.ns(doc)[xmlnsPrefix]}`);
 
     // Parse the SITEINFO element for metadata about the export.
-    const siteinfo = await this.parseElement(doc, 'mw:siteinfo');
+    const elSiteInfo = <any>(
+      await this.parseElement(doc, `${xmlnsPrefix}:siteinfo`)
+    );
     this.log(
-      `Loaded ${data.length} bytes exported from ${siteinfo.siteinfo.sitename} running ${siteinfo.siteinfo.generator}`
+      `Loaded ${buf.length} bytes exported from ${elSiteInfo.siteinfo.sitename} running ${elSiteInfo.siteinfo.generator}`
     );
 
     // Find all PAGE elements.
@@ -39,12 +41,12 @@ export default class Stamp extends Command {
   }
 
   ns(doc: libxmljs.Document): libxmljs.StringMap {
-    return { [Stamp.prefix]: <string>doc.root()?.namespace()?.href() };
+    return { [xmlnsPrefix]: <string>doc.root()?.namespace()?.href() };
   }
 
   async parseElement(doc: libxmljs.Document, xpath: string): Promise<object> {
     return await xml2js.parseStringPromise(
-      <string>this.getElement(doc, xpath).toString()
+      this.getElement(doc, xpath).toString()
     );
   }
 }
