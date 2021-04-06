8acb238ed18d5971527fbb40de8837378b9839bf
Cory Francis Myers <cfm@panix.com>
2021-04-06 14:35:23 -0700

    style: Prettier

--- src/commands/stamp.ts
+++ src/commands/stamp.ts
@@ -1,25 +1,29 @@
 import * as fs from 'fs';
 import * as libxmljs from 'libxmljs';
 import * as xml2js from 'xml2js';
-import { Command, flags } from '@oclif/command'
+import { Command, flags } from '@oclif/command';
 
 export default class Stamp extends Command {
-  static description = 'timestamp a MediaWiki export'
-  static args = [{ name: 'export', description: 'MediaWiki ".xml" export', required: true }]
+  static description = 'timestamp a MediaWiki export';
+  static args = [
+    { name: 'export', description: 'MediaWiki ".xml" export', required: true },
+  ];
 
   static prefix = 'mw';
 
   async run() {
-    const { args, flags } = this.parse(Stamp)
+    const { args, flags } = this.parse(Stamp);
 
     // Load and parse the ".xml" export.
-    const data = await fs.readFileSync(args.export)
+    const data = await fs.readFileSync(args.export);
     const doc = <libxmljs.Document>libxmljs.parseXml(data);
     this.debug(`xmlns=${this.ns(doc).mw}`);
 
     // Parse the SITEINFO element for metadata about the export.
     const siteinfo = await this.parseElement(doc, 'mw:siteinfo');
-    this.log(`Loaded ${data.length} bytes exported from ${siteinfo.siteinfo.sitename} running ${siteinfo.siteinfo.generator}`);
+    this.log(
+      `Loaded ${data.length} bytes exported from ${siteinfo.siteinfo.sitename} running ${siteinfo.siteinfo.generator}`
+    );
 
     // Find all PAGE elements.
     const pages = this.findElements(doc, '//mw:page');
@@ -39,6 +43,8 @@ export default class Stamp extends Command {
   }
 
   async parseElement(doc: libxmljs.Document, xpath: string): Promise<object> {
-    return await xml2js.parseStringPromise(<string>this.getElement(doc, xpath).toString());
+    return await xml2js.parseStringPromise(
+      <string>this.getElement(doc, xpath).toString()
+    );
   }
 }
